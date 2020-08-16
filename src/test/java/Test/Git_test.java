9413e79eaa19851063dadac8ab5bbfb2508d34e6
ratnadip Ghosh <ratnadipg@gmail.com>
2020-08-16 19:10:11 +0530

    Updated--> new Constructor added

--- src/test/java/Test/Git_test.java
+++ src/test/java/Test/Git_test.java
@@ -3,6 +3,12 @@ package Test;
 import org.testng.annotations.Test;
 
 public class Git_test {
+	
+	public Git_test()
+	{
+		System.out.println("in side the Constructor");
+	}
+	
 	@Test
 	public void testrepo()
 	{
